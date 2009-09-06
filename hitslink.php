fee147e31a2fe145a5a3dcd019f8f7fa152b8bb3
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-09-06 17:45:16 +0000

    release hitslink
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/hitslink/trunk@2574 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- hitslink.php
+++ hitslink.php
@@ -3,7 +3,7 @@
 Plugin Name: HitsLink
 Plugin URI: http://www.semiologic.com/software/hitslink/
 Description: Adds <a href="http://go.semiologic.com/hitslink">HitsLink</a> tracking to your site.
-Version: 2.0 RC3
+Version: 2.0
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: hitslink
